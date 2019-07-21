d0ebda1c1b66c15ad0821eabe0bd55449662fcdc
hahajing <592017023@qq.com>
2019-07-21 14:11:55 +0800

    本地工程1

--- src/main/java/cn/qi/Test.java
+++ src/main/java/cn/qi/Test.java
@@ -4,5 +4,10 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("eerwef");
+
+        int i=1;
+        i++;
+        System.out.println(i);
+
     }
 }
